29a5e5731b31a3a3345e331639081fe0e6b84c6a
Karl Altendorfer <karl.altendorfer@gmail.com>
2013-01-21 23:52:03 +0100

    Pair programming with ANDI

--- src/de/meineapp/EndlessInteger/EndlessIntCalc.java
+++ src/de/meineapp/EndlessInteger/EndlessIntCalc.java
@@ -3,86 +3,100 @@ import java.util.Arrays;
 
 public class EndlessIntCalc {
 	
+	private int[] saveNumber;
+	
+	
+	//Setter
+	public void setsaveNumber ( int[] saveNumber ) {
+		this.saveNumber = saveNumber;		
+	}
+	
+	
+	/**
+	 * Saves read in String to an Int Array
+	 * @param numberString
+	 */
+	public EndlessIntCalc ( String numberString ) {
+		
+		saveNumber = this.StringToIntArray(numberString);
+	}
+	
+	/**
+	 * Overwrites toString (which is always accessible, but does not work in most cases...)
+	 */
+	@Override public String toString(){
+		String result = "";
+		for ( int i = 0; i < saveNumber.length; i++){
+			result = "" + saveNumber[i] + result;		
+		}
+		return result;			
+	}
+	
+	
+	
+	
 	/**
 	 * Adds 2 Integers of random length
 	 * @param String endlessInteger1 -> Input Value
 	 * @param String endlessInteger2 -> Input Value
 	 * @return Calculated Integer
 	 */
-
-	public static String AddInteger ( String endlessInteger1, String endlessInteger2) {
-		//Get Length of longest String
-		int minStringLength = EndlessIntCalc.GetMinStringLength(endlessInteger1, endlessInteger2);
-		int maxArrayLength = EndlessIntCalc.GetMaxArrayLength(endlessInteger1, endlessInteger2);
-		
-		//Convert Strings to integer arrays
-		int[] integerArray1 = EndlessIntCalc.StringToIntArray(endlessInteger1);
-		int[] integerArray2 = EndlessIntCalc.StringToIntArray(endlessInteger2);
-		int resultArray[];
-		resultArray = new int [ maxArrayLength ];
-		
-		//Add element-wise
-		int override = 0;
-		for ( int i = 0; i < minStringLength; i++) {
-			if ([integerArray1[ maxArrayLength - i - 1 ] != null) & (integerArray2[ maxArrayLength - i - 1 ] != null) {
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-			}
-			
-			
-			
-			
-			//int sum = integerArray1[ maxArrayLength - i - 1 ]+ override + integerArray2[ maxArrayLength - i - 1 ];
-			if ( sum >= 10 ) {
-				override = 1;
-				resultArray[ (maxArrayLength - i -1) ] = sum-10;
-			}else {
-				resultArray[ (maxArrayLength - i -1) ] = sum;
-				override = 0;
-			}
-		
-			
-		}
-		String temp = Arrays.toString(resultArray).replace(", ", "");
-		String encoded = temp.substring(1, temp.length()-1);
-		return encoded;
-		
-		
-		
-		
-		}
-
 	
 	/**
-	 * Checks the minimum length of 2 entered Strings
-	 * @param String endlessInteger1 as Input Value 1
-	 * @param String endlessInteger2 as Input Value 2
-	 * @return Integer maximumStringLength
+	 * Getter--> LESEN!!!!
+	 * 
+	 * @return
 	 */
-	private static int GetMinStringLength (String endlessInteger1, String endlessInteger2) {
-		
-		int minStringLength = endlessInteger1.length();
-			if ( endlessInteger2.length() < minStringLength ) {
-				minStringLength = endlessInteger2.length();
-			}
-		return minStringLength;
+	public int[] getsaveNumber () {
+	return saveNumber;
 	}
-	
-	private static int GetMaxArrayLength (String endlessInteger1, String endlessInteger2) {
 		
-		int maxArrayLength = endlessInteger1.length();
-			if ( endlessInteger2.length() > maxArrayLength ) {
-				maxArrayLength = endlessInteger2.length();
-				//In case of override when both numbers are of same length
-				maxArrayLength = (maxArrayLength * 2) ;
+	
+	public EndlessIntCalc AddInteger ( EndlessIntCalc number ) {
+		int[] savedNumber = this.saveNumber;			//Objekt aus dem Konstruktor
+		int[] saved2Number = number.getsaveNumber();	//Zweites Objekt
+		
+		int maxLength = GetMaxArrayLength(savedNumber, saved2Number) + 1;
+		int[] additionResult = new int[maxLength];
+	
+			int rest = 0;
+			
+			int number1 = 0;
+			int number2 = 0;
+			
+			
+			for ( int i = 0; i < maxLength; i++) {
+				
+					
+					if ( i < savedNumber.length ) {
+						number1 = savedNumber[i];
+					}else {
+						number1 = 0;
+					}
+					if ( i < saved2Number.length ) {
+						number2 = saved2Number[i];
+					}else {
+						number2 = 0;
+					}
+					
+					additionResult[i] = (rest + number1 + number2) % 10;
+					rest = (rest + number1 + number2) / 10;
+					
+				
+			}
+			EndlessIntCalc result = new EndlessIntCalc("0");
+			result.setsaveNumber(additionResult);		
+			return result;	
+			}
+			
+			
+			
+
+	private int GetMaxArrayLength (int[] saveNumber, int[] save2Number) {
+		
+		int maxArrayLength = saveNumber.length;
+			if ( save2Number.length > saveNumber.length ) {
+				maxArrayLength = save2Number.length;
 			}
 		return maxArrayLength;
 	}
@@ -95,11 +109,11 @@ public class EndlessIntCalc {
 	 * @param return value
 	 * @return intArray INT Array holding all numbers of entered String
 	 */
-	private static int[]  StringToIntArray(String toConvert) {
+	private int[]  StringToIntArray(String toConvert) {
 		int[] intArray = new int[toConvert.length()];
 		 
-		for (int i = 0; i < toConvert.length(); i++) {
-			intArray[i] = Character.digit(toConvert.charAt(i), 10);
+		for (int i = toConvert.length() - 1; i >= 0; i--) {
+			intArray[toConvert.length() - i - 1] = Character.digit(toConvert.charAt(i), 10);
 		}
 		return intArray;
 
--- src/de/meineapp/EndlessInteger/Main.java
+++ src/de/meineapp/EndlessInteger/Main.java
@@ -9,14 +9,30 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bitte geben sie eine Beliebig lange Zahl1 ein: ");
+		/*System.out.println("Bitte geben sie eine Beliebig lange Zahl1 ein: ");
 		String endlessInteger1 = SavitchIn.readLine();
 		
 		System.out.println("Bitte geben sie eine Beliebig lange Zahl2 ein: ");
 		String endlessInteger2 = SavitchIn.readLine();
 		
-String result = EndlessIntCalc.AddInteger(endlessInteger1, endlessInteger2);
+
+		String result = EndlessIntCalc.AddInteger(endlessInteger1, endlessInteger2);
 		System.out.println("Die Summe der beiden Zahlen ist: " + result);
+		*/
+		
+		EndlessIntCalc test = new EndlessIntCalc("66");//put it up afterwards!!
+		System.out.println(test.toString());
+		
+		EndlessIntCalc test2 = new EndlessIntCalc("666");
+		System.out.println(test2.toString());
+		
+		EndlessIntCalc actualResult = test.AddInteger(test2);
+		System.out.println(actualResult);
+		
+		
+		
+		
+		
 		
 	}
 
