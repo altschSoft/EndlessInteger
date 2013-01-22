e8b23a91ae59458a116adcd93461321bf2d2c268
altschSoft <andi104@gmx.at>
2013-01-22 07:03:43 +0100

    Some comments

--- src/de/meineapp/EndlessInteger/EndlessIntCalc.java
+++ src/de/meineapp/EndlessInteger/EndlessIntCalc.java
@@ -3,11 +3,14 @@ import java.util.Arrays;
 
 public class EndlessIntCalc {
 	
+	//think more about your VariableNames --- too much saveNumbers and Numbers :)
+	
+	//Why is it better to have setter and getters and not a public int[]
 	private int[] saveNumber;
-	
-	
+		
 	//Setter
 	public void setsaveNumber ( int[] saveNumber ) {
+		//think about what happens when the saveNumber is null
 		this.saveNumber = saveNumber;		
 	}
 	
@@ -24,70 +27,66 @@ public class EndlessIntCalc {
 	/**
 	 * Overwrites toString (which is always accessible, but does not work in most cases...)
 	 */
-	@Override public String toString(){
+	@Override 
+	public String toString(){
 		String result = "";
 		for ( int i = 0; i < saveNumber.length; i++){
 			result = "" + saveNumber[i] + result;		
 		}
 		return result;			
-	}
-	
-	
-	
-	
-	/**
-	 * Adds 2 Integers of random length
-	 * @param String endlessInteger1 -> Input Value
-	 * @param String endlessInteger2 -> Input Value
-	 * @return Calculated Integer
-	 */
+	}	
+
 	
 	/**
 	 * Getter--> LESEN!!!!
 	 * 
 	 * @return
 	 */
-	public int[] getsaveNumber () {
-	return saveNumber;
+	public int[] getsaveNumber () {		
+		return saveNumber;
 	}
 		
 	
 	public EndlessIntCalc AddInteger ( EndlessIntCalc number ) {
-		int[] savedNumber = this.saveNumber;			//Objekt aus dem Konstruktor
+		//refactor not over 20 Lines in one methode		
+		int[] savedNumber = this.saveNumber;		//Objekt aus dem Konstruktor
 		int[] saved2Number = number.getsaveNumber();	//Zweites Objekt
 		
+		//GetMaxArrayLength is not the right name -- a name like GetMaxLengthOfTwoArrays would be better
 		int maxLength = GetMaxArrayLength(savedNumber, saved2Number) + 1;
 		int[] additionResult = new int[maxLength];
 	
-			int rest = 0;
-			
-			int number1 = 0;
-			int number2 = 0;
-			
-			
-			for ( int i = 0; i < maxLength; i++) {
-				
+		int rest = 0;
+		
+		int number1 = 0;
+		int number2 = 0;		
+		
+		for ( int i = 0; i < maxLength; i++) {
 					
-					if ( i < savedNumber.length ) {
-						number1 = savedNumber[i];
-					}else {
-						number1 = 0;
-					}
-					if ( i < saved2Number.length ) {
-						number2 = saved2Number[i];
-					}else {
-						number2 = 0;
-					}
-					
-					additionResult[i] = (rest + number1 + number2) % 10;
-					rest = (rest + number1 + number2) / 10;
-					
-				
+			//codeduplication :( make a function getNumberFromIntArraySavely
+			if ( i < savedNumber.length ) {
+				number1 = savedNumber[i];
+			}else {
+				number1 = 0;
 			}
-			EndlessIntCalc result = new EndlessIntCalc("0");
-			result.setsaveNumber(additionResult);		
-			return result;	
+			
+			if ( i < saved2Number.length ) {
+				number2 = saved2Number[i];
+			}else {
+				number2 = 0;
 			}
+			
+			additionResult[i] = (rest + number1 + number2) % 10;
+			rest = (rest + number1 + number2) / 10;
+				
+			
+		}
+		
+		//create a second construct that takes an int array
+		EndlessIntCalc result = new EndlessIntCalc("0");
+		result.setsaveNumber(additionResult);		
+		return result;	
+	}
 			
 			
 			
@@ -95,9 +94,9 @@ public class EndlessIntCalc {
 	private int GetMaxArrayLength (int[] saveNumber, int[] save2Number) {
 		
 		int maxArrayLength = saveNumber.length;
-			if ( save2Number.length > saveNumber.length ) {
-				maxArrayLength = save2Number.length;
-			}
+		if ( save2Number.length > saveNumber.length ) {
+			maxArrayLength = save2Number.length;
+		}
 		return maxArrayLength;
 	}
 	
@@ -105,19 +104,18 @@ public class EndlessIntCalc {
 	
 	/**
 	 * Converts a String to an IntArray
+	 * 
 	 * @param toConvert String Entered to be converted to an IntArray
 	 * @param return value
 	 * @return intArray INT Array holding all numbers of entered String
 	 */
 	private int[]  StringToIntArray(String toConvert) {
 		int[] intArray = new int[toConvert.length()];
-		 
+				
 		for (int i = toConvert.length() - 1; i >= 0; i--) {
 			intArray[toConvert.length() - i - 1] = Character.digit(toConvert.charAt(i), 10);
 		}
-		return intArray;
-
-		
+		return intArray;		
 		
 	}
 
